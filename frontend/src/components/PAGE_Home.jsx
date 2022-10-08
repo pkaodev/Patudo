7f3920d9d820d2b09a85c548ad12de93187cf583
pkaodev <polsson.development@gmail.com>
2022-10-09 00:10:41 +0100

    home message

--- frontend/src/components/PAGE_Home.jsx
+++ frontend/src/components/PAGE_Home.jsx
@@ -1,7 +1,22 @@
 export default function PAGE_Home() {
   return (
-    <>
-    HOME
-    </>
+    <div className="flex-column take-up-whole-space">
+    Welcome!
+    <br />
+    <br />
+    For the best experience, please view on a mobile device.
+    <br />
+    <br />
+    To play a game of Patudo, click on the "Play Patudo!" button in the menu, which can be accessed by clicking on  icon in the right corner of the screen.
+    <br />
+    <br />
+    Currently you will have to create an account to play, however a made up, non-existant email address will work.
+    <br />
+    <br />
+    If you are by yourself, create another account and playing against yourself.  You can start a game by yourself, but not a whole lot will happen.
+    <br />
+    <br />
+    Thank you for playing!
+    </div>
   );
 }
